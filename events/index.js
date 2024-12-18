8ef219feae0f61c0570be137208d44eb35f25edf
Prashantinfo <prashant.kaushik@infoobjects.com>
2024-12-18 14:56:07 +0530

    commit

--- events/index.js
+++ events/index.js
@@ -4,11 +4,11 @@
 
 
 // var eventEmitter =  new EventEmitter();
- 
+
 // var fun1 = (msg) => {
 //     console.log("Message from fun1: " + msg);
 // };
- 
+
 // var fun2 = (msg) => {
 //     console.log("Message from fun2: " + msg);
 // };
@@ -16,9 +16,9 @@
 // eventEmitter.on('myEvent', fun1);
 // eventEmitter.on('myEvent', fun1);
 // eventEmitter.on('myEvent', fun2);
- 
+
 // eventEmitter.removeListener('myEvent', fun1);
- 
+
 // eventEmitter.emit('myEvent', "Event occurred");
 
 // eventEmitter.removeAllListeners('myEvent');
@@ -35,7 +35,7 @@ const EventEmitter = require('events');
 var eventEmitter = new EventEmitter();
 
 eventEmitter.on('myEvent', (msg) => {
-    setImmediate( () => {
+    setImmediate(() => {
         console.log("Message from async: " + msg);
     });
 });
